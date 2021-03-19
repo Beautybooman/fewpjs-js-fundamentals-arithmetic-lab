fe2b7b88bd812bf226b77813eb697c37bb39872a
Isaiah Davis <ID@isaiahs-imac.myfiosgateway.com>
2021-03-19 17:28:08 -0400

    commit tree 5baa8417b67d2db898ef471d84699058b8aa09c4

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+const num1 = 31
+const num2 = 2
+const multiply = (num1 * num2)
 
-// Write your code here
+const random = Math.floor(Math.random() * 10)
+
+const num3 = 0
+const num4 = 4
+const mod = (num3 + num4)
+
+const max = Math.max(1, 2, 3, 4, 20)
+
+ 
+
+
+//function multiply ()
+//let result;
